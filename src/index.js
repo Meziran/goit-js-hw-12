c4aa543fbdef5b762008645ef35dc4ed2ec28691
Mateichuk_Oleksandr <71691100+Meziran@users.noreply.github.com>
2021-11-01 06:59:12 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import './styles.css';
 import countryCardsSucces from '../src/templates/success.hbs';
+import countryList from '../src/templates/error.hbs';
 import debounce from 'lodash.debounce';
 // import refs from './js/refs.js';
+// import '@pnotify/core/dist/BrightTheme.css';
+// import '@pnotify/core/dist/PNotify.css';
+// import { alert } from '@pnotify/core';
+import { showAlert, showError } from './pnofity';
+
 const refs = {
   seachForm: document.querySelector('#name_input'),
   // для вдагого
@@ -10,20 +16,34 @@ const refs = {
   listContainer: document.querySelector('.country_cards_list'),
 };
 
-refs.seachForm.addEventListener('input', debounce(seachContry, 500));
+refs.seachForm.addEventListener('input', debounce(seachContry, 1000));
 
 function seachContry(e) {
   e.preventDefault();
 
   const searchCountrys = refs.seachForm.value;
   featchCountrys(searchCountrys)
-    .then(renderContryCards)
-    .catch(error => console.log(error))
-    .finally(() => form.reset());
+    .then(country => {
+      if (country.length <= 2) {
+        renderContryCards(country);
+      } else if (2 < country.length <= 10) {
+        renderContryList(country);
+      }
+      if (country.length > 10) {
+        // errorContry();
+        return showAlert(`Too many matches found.Please enter a more specific query!`);
+      } else {
+        return errorContry;
+      }
+    })
+    // renderContryCards
+    // .then(renderContryList)
+    .catch(errorContry())
+    .finally(() => (refs.seachForm.value = ''));
 }
 
-function featchCountrys(country) {
-  return fetch(`https://restcountries.com/v2/name/${country}`).then(response => {
+function featchCountrys(name) {
+  return fetch(`https://restcountries.com/v2/name/${name}`).then(response => {
     return response.json();
   });
 }
@@ -33,9 +53,10 @@ function renderContryCards(country) {
   // const marckup = country.map(countryCardsSucces).join('');
   refs.cardContainer.innerHTML = marckup;
 }
-// function renderContryCards(country) {
-//   const markup = country.map(countryCardsSucces).join('');
-//   refs.cardContainer.innerHTML = markup;
-// }
-
-// peru
+function renderContryList(country) {
+  const marckup = countryList(country);
+  refs.cardContainer.innerHTML = marckup;
+}
+function errorContry(error) {
+  showError('This country not found');
+}
